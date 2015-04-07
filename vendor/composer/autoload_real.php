47b62352bf8f804e5aef3f0ebb59225743f6233e
Danny van Kooten <dannyvankooten@gmail.com>
2015-04-07 17:31:32 +0200

    Remove development dependencies from shipped autoloader.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,10 +23,6 @@ class ComposerAutoloaderInitc70faa45f4de0e3d0d59c20a20a2b55f
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitc70faa45f4de0e3d0d59c20a20a2b55f', 'loadClassLoader'));
 
-        $includePaths = require __DIR__ . '/include_paths.php';
-        array_push($includePaths, get_include_path());
-        set_include_path(join(PATH_SEPARATOR, $includePaths));
-
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
