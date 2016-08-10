a70d4d4e4da36588a666f4f39265088c52d9cf67
Gabor Javorszky <gabor@javorszky.co.uk>
2016-08-10 22:55:03 +0100

    Added three extra public methods for more flexibility
    This way you can not only declare the only plugins that should run on a given request, but you can add, and remove certain plugins from the ones that are normally there.

--- src/Endpoint.php
+++ src/Endpoint.php
@@ -15,11 +15,22 @@ class Endpoint {
 	public $active_plugins = array();
 
 	/**
+	 * @var string string to denote the mode of the plugins. Add to, remove from, or only those declared.
+	 */
+	public $mode = 'only';
+
+	/**
 	 * @param string $url
 	 * @param array  $plugins
 	 */
-	public function __construct( $url = '/', array $plugins = array() ) {
+	public function __construct( $url = '/', array $plugins = array(), $mode = 'only' ) {
 		$this->url = $url;
 		$this->active_plugins = $plugins;
+
+		if ( ! is_string( $mode ) || ! in_array( $mode, array( 'only', 'add', 'remove' ) ) ) {
+			$this->mode = 'only';
+		} else {
+			$this->mode = $mode;
+		}
 	}
 }
